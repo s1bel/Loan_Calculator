79156ca959451ea1acc86343fc6cb5e189d65a51
s1bel <ilya.koshkarov@gmail.com>
2020-10-07 16:21:01 +0700

    all done, it works somehow...

--- Loan Calculator/task/creditcalc/creditcalc.py
+++ Loan Calculator/task/creditcalc/creditcalc.py
@@ -1,17 +1,5 @@
-# import argparse
 import math
 import sys
-
-
-# parser = argparse.ArgumentParser()
-# parser.add_argument('--type', choices=['annuity', 'diff'], help='The type of payment: "annuity" or "diff" (differentiated)')
-# parser.add_argument('--payment', help='The monthly payment amount. Use only with --type=annuity.')
-# parser.add_argument('--principal', help='is used for calculations of both types of payment')
-# parser.add_argument('--periods', help='denotes the number of months needed to repay the loan')
-# parser.add_argument('--interest', help='is specified without a percent sign')
-# args = parser.parse_args()
-# print(args)
-# exit(1)
 
 args = sys.argv
 formatted_args = []
@@ -19,88 +7,81 @@ formatted_args = []
 if len(args) > 4:
     for i in args:
         formatted_args += i.split('=')
-else:
-    print('Incorrect parameters')
-    exit(0)
+    if ('--payment' in formatted_args
+        and float(formatted_args[formatted_args.index('--payment') + 1]) < 0) \
+            or ('--principal' in formatted_args
+                and float(formatted_args[formatted_args.index('--principal') + 1]) < 0) \
+            or ('--periods' in formatted_args
+                and float(formatted_args[formatted_args.index('--periods') + 1]) < 0) \
+            or ('--interest' in formatted_args
+                and float(formatted_args[formatted_args.index('--interest') + 1]) < 0):
+        print('Incorrect parameters')
+    elif '--type' in formatted_args and ('diff' in formatted_args or 'annuity' in formatted_args) \
+            and '--interest' in formatted_args:
+        interest = float(formatted_args[formatted_args.index('--interest') + 1])
+        nom_interest_rate = interest / (12 * 100)
 
-if '--payment' in formatted_args and float(formatted_args[formatted_args.index('--payment') + 1]) < 0:
-    print('Incorrect parameters')
-    exit(0)
-elif '--principal' in formatted_args and float(formatted_args[formatted_args.index('--principal') + 1]) < 0:
-    print('Incorrect parameters')
-    exit(0)
-elif '--periods' in formatted_args and float(formatted_args[formatted_args.index('--periods') + 1]) < 0:
-    print('Incorrect parameters')
-    exit(0)
-elif '--interest' in formatted_args and float(formatted_args[formatted_args.index('--interest') + 1]) < 0:
-    print('Incorrect parameters')
-    exit(0)
+        if 'diff' in formatted_args and '--payment' not in formatted_args:
+            principal = float(formatted_args[formatted_args.index('--principal') + 1])
+            periods = int(formatted_args[formatted_args.index('--periods') + 1])
+            payment_sum = 0
+            for m in range(1, periods + 1):
+                payment = float(principal / periods + nom_interest_rate * (principal - (principal * (m - 1)) / periods))
+                if payment % 1:
+                    payment = int(payment) + 1
+                else:
+                    payment = int(payment)
+                payment_sum += payment
+                print(f'Month {m}: payment is {payment}')
+            overpayment = int(payment_sum - principal)
+            print()
+            print('Overpayment =', overpayment)
+        elif 'annuity' in formatted_args and '--periods' not in formatted_args:
+            principal = float(formatted_args[formatted_args.index('--principal') + 1])
+            payment = float(formatted_args[formatted_args.index('--payment') + 1])
+            overpayment = - principal
+            periods = math.log(payment / (payment - nom_interest_rate * principal), (1 + nom_interest_rate))
+            if periods % 1 == 0:
+                periods = int(periods)
+            else:
+                periods = int(periods) + 1
+            years = periods // 12
+            months = periods % 12
+            if years == 0:
+                print(
+                    'It will take ' + str(months) + (' month' if months == 1 else ' months') + ' to repay the loan!')
+            elif months == 0:
+                print('It will take ' + str(years) + (' year' if years == 1 else ' years') + ' to repay the loan!')
+            else:
+                print('It will take ' + str(years) + (' year' if years == 1 else ' years') + ' and '
+                      + str(months) + (' month' if months == 1 else ' months') + ' to repay the loan!')
+            overpayment = int(payment * periods - principal)
+            print('Overpayment =', overpayment)
+        elif 'annuity' in formatted_args and '--payment' not in formatted_args:
+            principal = float(formatted_args[formatted_args.index('--principal') + 1])
+            periods = int(formatted_args[formatted_args.index('--periods') + 1])
+            payment = principal * (
+                            (nom_interest_rate * math.pow(1 + nom_interest_rate, periods))
+                            / (math.pow(1 + nom_interest_rate, periods) - 1))
+            if payment % 1:
+                payment = int(payment) + 1
 
-if '--type' in formatted_args and ('diff' in formatted_args or 'annuity' in formatted_args) and '--interest' in formatted_args:
-    if 'diff' in formatted_args and not '--payment' in formatted_args:
-        pass
-    elif 'annuity' in formatted_args:
-        pass
+            print(f'Your annuity payment = {payment}')
+            #        last_payment = loan_principal - (months - 1) * payment
+            #        print('Your monthly payment = ' + str(payment)
+            #        + ' and the last payment = ' + str(last_payment) + '.')
+            overpayment = int(payment * periods - principal)
+            print('Overpayment =', overpayment)
+        elif 'annuity' in formatted_args and '--principal' not in formatted_args:
+            periods = int(formatted_args[formatted_args.index('--periods') + 1])
+            payment = float(formatted_args[formatted_args.index('--payment') + 1])
+            principal = int(payment / (
+                            (nom_interest_rate * math.pow(1 + nom_interest_rate, periods))
+                            / (math.pow(1 + nom_interest_rate, periods) - 1)))
+            print(f'Your loan principal = {principal}!')
+            overpayment = int(payment * periods - principal)
+            print('Overpayment =', overpayment)
     else:
         print('Incorrect parameters')
-        exit(0)
 else:
     print('Incorrect parameters')
-    exit(0)
-
-exit(0)
-
-print('''What do you want to calculate?
-type "n" - for number of monthly payments,
-type "a" - for annuity monthly payment amount,
-type "p" - for the monthly payment:''')
-user_choice = input()
-if user_choice == 'n':
-    print('Enter the loan principal:')
-    loan_principal = float(input())
-    print('Enter the monthly payment:')
-    monthly_payment = float(input())
-    print('Enter the loan interest:')
-    loan_interest = float(input())
-    nominal_interest_rate = loan_interest / (12 * 100)
-    months = math.log(monthly_payment
-                       / (monthly_payment - nominal_interest_rate * loan_principal), (1 + nominal_interest_rate))
-    if months % 1 == 0:
-        months = int(months)
-    else:
-        months = int(months) + 1
-    years = months // 12
-    months = months % 12
-    if years == 0:
-        print('It will take ' + str(months) + (' month' if months == 1 else ' months') + ' to repay the loan')
-    else:
-        print('It will take ' + str(years) + (' year' if months == 1 else ' years') + ' and ' + str(months) + (
-            ' month' if months == 1 else ' months') + ' to repay the loan')
-elif user_choice == 'a':
-    print('Enter the loan principal:')
-    loan_principal = float(input())
-    print('Enter the number of periods:')
-    months = int(input())
-    print('Enter the loan interest:')
-    loan_interest = float(input())
-    nominal_interest_rate = loan_interest / (12 * 100)
-    monthly_payment = loan_principal * ((nominal_interest_rate * math.pow(1 + nominal_interest_rate, months))
-                                        / (math.pow(1 + nominal_interest_rate, months) - 1))
-    if monthly_payment % 1 == 0:
-        print('Your monthly payment = ' + str(int(monthly_payment)))
-    else:
-        monthly_payment = int(monthly_payment) + 1
-        print('Your monthly payment = ' + str(int(monthly_payment)))
-#        last_payment = loan_principal - (months - 1) * payment
-#        print('Your monthly payment = ' + str(payment) + ' and the last payment = ' + str(last_payment) + '.')
-elif user_choice == 'p':
-    print('Enter the annuity payment:')
-    monthly_payment = float(input())
-    print('Enter the number of periods:')
-    months = int(input())
-    print('Enter the loan interest:')
-    loan_interest = float(input())
-    nominal_interest_rate = loan_interest / (12 * 100)
-    loan_principal = monthly_payment / ((nominal_interest_rate * math.pow(1 + nominal_interest_rate, months))
-                                        / (math.pow(1 + nominal_interest_rate, months) - 1))
-    print(f'Your loan principal = {loan_principal:.0f}')
